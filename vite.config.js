a3b32c164d22b96fae7246d455cf44d6bdecd1ce
siegfrid1994-a11y <siegfrid1994@gmail.com>
2026-07-15 00:52:51 +0300

    Fix Vite React JSX runtime

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/medvezhonok/',
+  plugins: [
+    react({
+      jsxRuntime: 'automatic',
+    }),
+  ],
+  esbuild: {
+    jsx: 'automatic',
+  },
   build: {
     assetsDir: 'assets',
     sourcemap: true,
